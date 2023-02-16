f74f69719c5313d476fa465ae1bd2953a2a087e9
Tracey-Lee Ash <traceyleeash3017@gmail.com>
2023-02-16 13:56:58 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - A program that prints a linr using printf function
+* Return 0 (Success)
+*/
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
