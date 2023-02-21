435b0cd1b4162b73142ce4a1d06f68947190fd6b
Tracey-Lee Ash <traceyleeash3017@gmail.com>
2023-02-21 11:30:45 -0800

    Main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* 
+ * Header file containing declarations for all function used in the 0x02- * functions_nest_loops directory.
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
